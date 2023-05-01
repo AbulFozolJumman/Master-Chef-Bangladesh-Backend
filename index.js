68cdf6ba1cf9920231c94628698c8c07849ccb36
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-05-01 23:18:03 +0600

    nodemon installation done

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hellooo World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example appp listening on port ${port}`)
 })
