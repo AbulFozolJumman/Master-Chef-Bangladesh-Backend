849fcb832bed212aa728b67e177f67b16e0227ca
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-05-02 12:44:31 +0600

    All the chefs data updated to vercel and express

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000 
+const port = 3000;
+const chefs = require('./chefs.json');
 
 app.get('/', (req, res) => {
   res.send('Hellooo World!')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
 })
 
 app.listen(port, () => {
